3f21d3753fb6232d4e8e55f61f7daa02ca68286c
Abdwahab Abdsalam Babatunde <138698588+Tunenpo@users.noreply.github.com>
2023-09-25 13:51:03 +0100

    Primitive Variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,21 @@
 public class HelloWorld {
     //Call to main
     public static void main(String[] args) {
+        //Declaration and Initialization of Variable
+        int myAge = 40;
+        int herAge = myAge;
+        char alpha = 'b';
+        float num = 6.456f;
+        byte aByte = 127;
+        boolean see = false;
         //Print statement
+        System.out.println(alpha);
+        System.out.println(herAge);
+        System.out.println(num);
+        System.out.println(myAge);
+        System.out.println(aByte);
         System.out.println("Hello world, this is my first java programm");
+        System.out.println(see);
     }
 
 }
