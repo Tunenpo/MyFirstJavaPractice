4406476e1202daa3ef79bcb12c49d99453e4274d
Abdwahab Abdsalam Babatunde <138698588+Tunenpo@users.noreply.github.com>
2023-09-27 21:24:52 +0100

    loop

---
+++ src/loop.java
@@ -0,0 +1,13 @@
+public class loop {
+    public static void main(String[] args) {
+        int myNumber = 25;
+        
+        while(myNumber < 50) {
+        System.out.println("okay" + " " + myNumber);
+
+        myNumber = myNumber +5;
+        
+       } 
+
+    }
+}
