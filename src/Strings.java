68cf51ad5ab1176118fdefeaf1cf10ae9a425cb7
Abdwahab Abdsalam Babatunde <138698588+Tunenpo@users.noreply.github.com>
2023-09-27 12:25:12 +0100

    Strings in java to write multiple of text

---
+++ src/Strings.java
@@ -0,0 +1,32 @@
+public class Strings {
+    public static void main(String[] args) {
+
+        //Declaration of int variabales
+        int myNum = 6;
+        //Declaration of variable using double datatypes
+        double myDouble = 9.4321;
+        //Declaration of variable using String to print out text
+        String text = "I love java";
+        //Declaration of variable creating a space using blank
+        String blank = " ";
+        String moreText = "Java seems the best out of all programming language";
+        //Declaration a varaible combining all variables declared into a single variable
+        String compound = text + blank + moreText;
+        System.out.println( );
+        System.out.println(myNum);
+        System.out.println();
+        System.out.println(myDouble);
+        System.out.println();
+        System.out.println(text);
+        System.out.println(blank);
+        System.out.println(moreText);
+        System.out.println();
+        System.out.println(compound);
+        System.out.println();
+        System.out.println("I love java" + " " + "Java seems the best out of all programming language");
+        System.out.println();
+        System.out.println("My love for java:" + myNum + " ," + myDouble);
+    }
+
+    
+}
