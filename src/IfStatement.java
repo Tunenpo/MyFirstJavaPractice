7c31e2df09fbb90f080f89ffc3318b87eb396ec2
Abdwahab Abdsalam Babatunde <138698588+Tunenpo@users.noreply.github.com>
2023-09-28 21:53:42 +0100

    if, else if and else in java

---
+++ src/IfStatement.java
@@ -0,0 +1,17 @@
+public class IfStatement {
+    public static void main(String[] args) {
+        int myAmount = 20;
+        if (myAmount < 22) {
+            System.out.println("It's True");
+        }
+            else if (myAmount > 30) {
+                System.out.println("It's False");
+            }
+            else{
+                System.out.println("None of the above");
+            }
+
+    
+    }
+    
+}
