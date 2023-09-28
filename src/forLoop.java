01484d854764ce788ee5c2ffa9d4d04f308ea176
Abdwahab Abdsalam Babatunde <138698588+Tunenpo@users.noreply.github.com>
2023-09-28 11:07:03 +0100

    forLoop in java

---
+++ src/forLoop.java
@@ -0,0 +1,15 @@
+public class forLoop {
+    public static void main(String[] args) {
+        //Declaring and integer,initializing and increment forLopp
+        for(int i=0;i<10;i++) {
+            //Using printf function because you need to print out the value of inetger
+            System.out.printf("Java is becoming more interesting by %d\n", i);
+            
+            System.out.println();
+            
+            //Using the standard java function to print out with its increment value
+            System.out.println("java is becoming more interesting by " + i); 
+        }
+    }
+
+}
