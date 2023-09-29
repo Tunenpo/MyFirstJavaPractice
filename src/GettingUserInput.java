66fb7b6719d8cf081761869e8e73632fd683a85d
Abdwahab Abdsalam Babatunde <138698588+Tunenpo@users.noreply.github.com>
2023-09-29 21:50:55 +0100

    Getting User Input in Java

--- src/GettingUserInput.java
+++ src/GettingUserInput.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 
 public class GettingUserInput {
     public static void main(String[] args) {
-        Scanner input = 
-    
+        
+        Scanner input = new Scanner (System.in);
+
+        System.out.println("Input your value");
+
+        int aValue = input.nextInt();
+
+        System.out.println("Your value is: " + aValue);
     }
     
 }
