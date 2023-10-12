e9083a5bcce1e0bfffdbbd011d1244c4ff257f77
Abdwahab Abdsalam Babatunde <138698588+Tunenpo@users.noreply.github.com>
2023-10-12 12:19:03 +0100

    casting of data types in java

---
+++ src/Casting.java
@@ -0,0 +1,23 @@
+public class Casting {
+
+    public static void main(String[] args) {
+        
+        byte byteValue = 3;
+        short shortValue = 38;
+        int intValue = 29;
+        long longValue = 90324;
+
+        float floatValue = 923.5f;
+        double doubleValue = 2341.4;
+
+        System.out.println(Byte.MAX_VALUE);
+
+        intValue = (int)longValue;
+        System.out.println(intValue);
+
+        intValue = (int)doubleValue;
+        System.out.println(intValue);
+
+    }
+    
+}
